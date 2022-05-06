248a1724e884e5cbe01fbe6506fc6471f06f1cfa
Elian de Oliveira Moraes <85230407+ElianMoraes@users.noreply.github.com>
2022-05-06 11:30:07 -0300

    Add files via upload

---
+++ Mobile/pages2/home/style.js
@@ -0,0 +1,317 @@
+//  import React from 'react';
+// import { StyleSheet } from 'react-native';
+
+// export default StyleSheet.create({
+//     body: {
+//         flex: 1,
+//         alignItems: "center",
+//         backgroundColor: 'white',
+//     },
+
+//     perfil: {
+//         width: 50,
+//         marginTop: 20,
+//         marginRight: 20,
+//         height: 50,
+//         borderRadius: 50
+//     },
+
+//     card: {
+//         zIndex: -1,
+//         marginBottom: 30,
+//         width: "95%",
+//         height: 150,
+//         marginTop: 20,
+//         justifyContent: "space-around",
+//         flexDirection: "row",
+//         alignItems: "flex-end",
+//         borderWidth: 2,
+//         alignSelf: "center",
+//         flexDirection: "column"
+//         // borderTopLeftRadius: 20,
+//         // borderTopRightRadius: 20,
+//     },
+
+//     pesq: {
+//         position:"relative",
+//         flexDirection: "column",
+//         maxWidth: 235,
+//         height:"auto",
+//         alignItems: "center"
+//     },
+
+//     input: {
+//         width: "100%",
+//         height: "100%",
+//     },
+
+//     header: {
+//         width: "100%",
+//         height: 100,
+//         position: 'relative',
+//         alignItems: 'center',
+//         flexDirection: 'row',
+//         justifyContent: 'space-between',
+//         backgroundColor: 'purple'
+//     },
+
+//     footer: {
+//        width: "100%",
+//        height: 150,
+//        alignItems: 'center',
+//        justifyContent: 'space-around',
+//        backgroundColor: "purple",
+//        bottom: 0,
+//        position: "absolute"
+//     },
+
+//     logo: {
+//         marginTop: 18,
+//         padding: 0,
+//         width: 70,
+//         height: 70,
+//     },
+
+//     menu: {
+//         width: 40,
+//         height: 40,
+//         marginLeft: 10,
+//         marginTop: 20
+//     },
+
+//     enviar: {
+//         width: 30,
+//         height: 30,
+//         marginLeft: 10
+//     },
+
+//     text: {
+//         fontSize: 17,
+//         color: 'white',
+//         textAlign: 'center'
+//     },
+
+//     face: {
+//         width: 50,
+//         height: 50
+//     },
+
+//     twitter: {
+//         width: 50,
+//         height: 50
+//     },
+
+//     insta: {
+//         width: 50,
+//         height: 50
+//     },
+
+//     imagefooter: {
+//         width: "100%",
+//         flexDirection: 'row',
+//         justifyContent: 'space-evenly'
+//     },
+
+//     like: {
+//         width: 30,
+//         height: 30,
+//     },
+
+//     deslike: {
+//         width: 30,
+//         height: 30,
+//     },
+
+//     scroll: {
+//         width: "100%",
+//         height: 650
+//     },
+
+//     menu: {
+//         marginLeft: 10,
+//         width: 40,
+//         height: 40,
+//         marginTop: 20
+//     },
+
+//     buscar: {
+//         backgroundColor: 'white',
+//         width: 235,
+//         padding: 5,
+//         top: 10
+//     },
+
+//     procurar: {
+//         width: 25,
+//         height: 25,
+//         marginTop: 18
+//     },
+
+//     Open:{
+//         flex:1,
+//         width: "100%",
+//         maxHeight: 250,
+//     },
+
+//     notOpen: {
+//         display: 'none',
+//         flex:1,
+//         width: "100%",
+//         maxHeight: 250,
+//     }
+    
+// });
+
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    body: {
+        flex: 1,
+        alignItems: "center",
+        backgroundColor: 'black'
+    },
+
+    ceu: {
+        width: "100%",
+        height: 345   
+    },
+
+    card : {
+        width: "30%",
+        height: "17%",
+        marginTop: 30,
+        borderRadius: 100,
+        alignSelf: "center",
+    },
+
+    header: {
+        width: "100%",
+        height: 100,
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor: 'purple'
+    },
+
+    footer: {
+        width: "100%",
+        marginTop: 100,
+        height: 150,
+        backgroundColor: "purple",
+    },
+
+    logo: {
+        marginTop: -45,
+        marginLeft: 320,
+        padding: 0,
+        width: 70,
+        height: 70,
+    },
+
+    menu: {
+        width: 40,
+        height: 30,
+        marginLeft: 10,
+        marginTop: -300
+    },
+
+    text: {
+        fontSize: 17,
+        color: 'white',
+        textAlign: 'center'
+    },
+
+    face: {
+        width: 50,
+        height: 50
+    },
+
+    twitter: {
+        width: 50,
+        height: 50
+    },
+
+    insta: {
+        width: 50,
+        height: 50
+    },
+
+    imagefooter: {
+        width: "100%",
+        height: "15%",
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        alignItems: "center",
+        marginTop: "8%",
+        marginBottom: "8%"
+    },
+
+    card1: {
+        width: 80,
+        marginRight: 20,
+        padding: 15,
+        paddingTop: 0,
+        paddingBottom: 0,
+        borderRadius: 5,
+        height: "100%",
+        alignSelf: "center"
+    },
+
+    org: {
+        margin: 30,
+        width: "100%",
+        height: 100,
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        alignSelf: "center"
+    },
+
+    textin: {
+        marginTop: 150,
+        marginRight: 310,
+        fontSize: 17,
+        alignSelf: "center",
+        fontWeight: "bold",
+        color: "white"
+    },
+
+    texto: {
+        fontSize: 30,
+        marginRight: 150,
+        marginTop: -140,
+        color: 'white',
+    },
+
+    texto2: {
+        fontSize: 15,
+        marginRight: 150,
+        marginTop: 10,
+        color: 'white',
+    },
+
+    scroll: {
+        width: "100%",
+        height: 775,
+    },
+
+    perfil: {
+        width: 120,
+        height: 120,
+        borderRadius: 60,
+    },
+
+    harry: {
+        width: 85,
+        height: 120,
+    },
+
+    lados: {
+        width: 70,
+        marginRight: 20,
+        height: "100%",
+        alignSelf: "center",
+    }
+});
