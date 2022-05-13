3a16eed3524a7e899176b59590526864c2b9c193
Elian de Oliveira Moraes <85230407+ElianMoraes@users.noreply.github.com>
2022-05-13 11:25:18 -0300

    Add files via upload

---
+++ Mobile/pages2/home/style.js
@@ -0,0 +1,169 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    body: {
+        flex: 1,
+        alignItems: "center",
+        backgroundColor: '#0a0c0d'
+    },
+
+    ceu: {
+        width: "100%",
+        height: 350   
+    },
+    
+    pesq: {
+        position:"relative",
+        flexDirection: "column",
+        maxWidth: 235,
+        height:"auto",
+        alignItems: "center",
+    },
+
+    buscar: {
+        backgroundColor: 'white',
+        width: 220,
+        marginLeft: 150,
+        marginTop: -295,
+        padding: 5,
+    },
+
+    card : {
+        width: "30%",
+        height: "17%",
+        marginTop: 30,
+        borderRadius: 100,
+        alignSelf: "center",
+    },
+
+    header: {
+        width: "100%",
+        height: 100,
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor: 'purple'
+    },
+
+    footer: {
+        width: "100%",
+        marginTop: 100,
+        height: 150,
+        backgroundColor: "black",
+    },
+
+    logo: {
+        marginTop: -55,
+        marginLeft: 320,
+        padding: 0,
+        width: 70,
+        height: 80,
+    },
+
+    menu: {
+        width: 45,
+        height: 30,
+        marginLeft: 10,
+        marginTop: -290
+    },
+
+    text: {
+        fontSize: 17,
+        color: 'white',
+        textAlign: 'center'
+    },
+
+    face: {
+        width: 50,
+        height: 50
+    },
+
+    twitter: {
+        width: 50,
+        height: 50
+    },
+
+    insta: {
+        width: 50,
+        height: 50,
+    },
+
+    imagefooter: {
+        width: "100%",
+        height: "15%",
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        alignItems: "center",
+        marginTop: "8%",
+        marginBottom: "8%"
+    },
+
+    card1: {
+        width: 80,
+        marginRight: 20,
+        padding: 15,
+        paddingTop: 0,
+        paddingBottom: 0,
+        borderRadius: 5,
+        height: "100%",
+        alignSelf: "center"
+    },
+
+    org: {
+        margin: 10,
+        width: "100%",
+        height: 100,
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        alignSelf: "center"
+    },
+
+    textin: {
+        marginTop: 80,
+        marginRight: 310,
+        fontSize: 17,
+        alignSelf: "center",
+        fontWeight: "bold",
+        color: "white"
+    },
+
+    texto: {
+        fontSize: 30,
+        marginRight: 150,
+        marginTop: -140,
+        color: 'white',
+    },
+
+    texto2: {
+        fontSize: 15,
+        marginRight: 150,
+        marginTop: 2,
+        color: 'white',
+    },
+
+    scroll: {
+        width: "100%",
+        height: 775,
+    },
+
+    perfil: {
+        width: 120,
+        height: 120,
+        borderRadius: 60,
+    },
+
+    harry: {
+        width: 85,
+        height: 120,
+    },
+
+    lados: {
+        width: 70,
+        marginRight: 20,
+        height: "100%",
+        alignSelf: "center",
+    }
+});
