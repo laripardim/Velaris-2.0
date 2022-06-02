a39e49ec40efe055f4b04e141bf43480b6b15d81
Elian de Oliveira Moraes <85230407+ElianMoraes@users.noreply.github.com>
2022-06-02 08:21:36 -0300

    Add files via upload

---
+++ Mobile/pages2/leitura/index.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import { View, Image, Text, ScrollView, TouchableOpacity } from 'react-native';
+import style from './style.js'
+
+export default function Leitura({ navigation, route }) {
+    // const {id} = route.params
+
+    // useEffect(() => {
+    //     fetch('http://10.87.207.23:7000/leitura', {
+    //         "method": "GET",
+    //         "headers": {
+    //             "Content-Type":"application/json"
+    //         }
+    //     })
+    // })
+
+    return (
+        <View style={style.body}>
+            <View style={style.scroll}>
+                <ScrollView>
+                    <Image style={style.ceu} source={require("../../assets/leituralivro.png")}></Image>
+                    <TouchableOpacity onPress={() => { navigation.openDrawer() }}>
+                        <Image style={style.menu} source={require("../../assets/menu.png")}></Image>
+                        <Image style={style.logo} source={require('../../assets/LOGO.png')}></Image>
+                    </TouchableOpacity>
+                    <View style={style.card}>
+                        <Text style={{color: 'white', fontSize:20}}>{}</Text>
+                    </View>
+                    <View style={style.footer}>
+                        <Text style={style.text}>Nos siga nas redes sociais !</Text>
+                        <View style={style.imagefooter}>
+                            <Image style={style.face} source={require('../../assets/facebook.png')}></Image>
+                            <Image style={style.twitter} source={require('../../assets/twitter.png')}></Image>
+                            <Image style={style.insta} source={require('../../assets/insta.png')}></Image>
+                        </View>
+                        <Text style={style.text}>Developed by Elian, Larissa e Lavínia</Text>
+                    </View>
+                </ScrollView>
+            </View>
+        </View>
+    );
+}
