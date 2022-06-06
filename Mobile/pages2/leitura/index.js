7e16d319b901c04f85b4ff095792c0f1129cd8df
Elian de Oliveira Moraes <85230407+ElianMoraes@users.noreply.github.com>
2022-06-06 11:29:25 -0300

    Add files via upload

---
+++ Mobile/pages2/leitura/index.js
@@ -0,0 +1,54 @@
+import React, { useEffect } from 'react';
+import { View, Image, Text, ScrollView, TouchableOpacity } from 'react-native';
+import style from './style.js'
+import * as Speech from 'expo-speech';
+
+export default function Leitura({ navigation, route }) {
+    const {texto} = route.params
+    const speak = () => {
+        const thingToSay = texto;
+        Speech.speak(thingToSay);
+      };
+
+    useEffect(() => {
+        console.log("TEXTO do TEXTO ", texto);
+        fetch('http://10.87.207.23:7000/leitura', {
+            "method": "GET",
+            "headers": {
+                "Content-Type":"application/json"
+            }
+        })
+    },[]);
+
+    return (
+        <View style={style.body}>
+            <View style={style.scroll}>
+                <ScrollView>
+                    <Image style={style.ceu} source={require("../../assets/leituralivro.png")}></Image>
+                    <TouchableOpacity onPress={() => { navigation.openDrawer() }}>
+                        <Image style={style.menu} source={require("../../assets/menu.png")}></Image>
+                        <Image style={style.logo} source={require('../../assets/LOGO.png')}></Image>
+                    </TouchableOpacity>
+                    <View>
+                    </View>
+                    <TouchableOpacity onPress={speak}>
+                        <Text style={{color: 'white', textAlign: 'center', fontSize: 18}}>Ouça o Livro:</Text>
+                        <Image source={require('../../assets/volume.png')} style={{width: 32, height: 32, marginLeft: 175, marginTop: 25}} />
+                    </TouchableOpacity>
+                    <View style={style.card}>
+                        <Text style={{color: 'white', fontSize:20}}>{texto}</Text>
+                    </View>
+                    <View style={style.footer}>
+                        <Text style={style.text}>Nos siga nas redes sociais !</Text>
+                        <View style={style.imagefooter}>
+                            <Image style={style.face} source={require('../../assets/facebook.png')}></Image>
+                            <Image style={style.twitter} source={require('../../assets/twitter.png')}></Image>
+                            <Image style={style.insta} source={require('../../assets/insta.png')}></Image>
+                        </View>
+                        <Text style={style.text}>Developed by Elian, Larissa e Lavínia</Text>
+                    </View>
+                </ScrollView>
+            </View>
+        </View>
+    );
+}
---
+++ Mobile/pages2/perfil/style.js
@@ -0,0 +1,156 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    body: {
+        flex: 1,
+        height: 1360,
+        alignItems: "center",
+        backgroundColor: '#0a0c0d'
+    },
+
+    ceu: {
+        width: "100%",
+        height: 350   
+    },
+
+    logo: {
+        marginTop: -55,
+        marginLeft: 320,
+        padding: 0,
+        width: 70,
+        height: 80,
+    },
+
+    menu: {
+        width: 45,
+        height: 30,
+        marginLeft: 10,
+        marginTop: -290
+    },
+
+    scroll: {
+        width: "100%",
+        height: 775,
+    },
+
+    texto: {
+        fontSize: 30,
+        marginRight: 150,
+        marginTop: -140,
+        color: 'white',
+    },
+
+    texto2: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginRight: 150,
+        marginTop: 2,
+        color: 'white',
+    },
+
+    card : {
+        width: "30%",
+        height: "17%",
+        marginTop: 50,
+        borderRadius: 100,
+        alignSelf: "center",
+    },
+
+    perfil: {
+        width: 150,
+        height: 150,
+        right: 15,
+        borderRadius: 100,
+    },
+
+    textin: {
+        bottom: 30,
+        fontSize: 18,
+        position: 'relative',
+        alignSelf: "center",
+        fontWeight: "bold",
+        color: 'white'
+    },
+
+    textin1: {
+        marginTop: 25,
+        alignSelf: "center",
+        fontWeight: "bold",
+        color: 'white'
+    },
+
+    org: {
+        margin: 40,
+        width: "100%",
+        height: 100,
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        alignSelf: "center"
+    },
+
+    lados: {
+        width: 70,
+        marginRight: 20,
+        height: "100%",
+        alignSelf: "center",
+    },
+
+    card1: {
+        width: 80,
+        marginRight: 20,
+        padding: 15,
+        paddingTop: 0,
+        paddingBottom: 0,
+        borderRadius: 5,
+        height: "100%",
+        alignSelf: "center"
+    },
+
+    harry: {
+        width: 78,
+        height: 98,
+    },
+
+    footer: {
+        width: "100%",
+        marginTop: 50,
+        height: 200,
+        backgroundColor: "black",
+    },
+
+    text: {
+        fontSize: 17,
+        color: 'white',
+        textAlign: 'center'
+    },
+
+    imagefooter: {
+        width: "100%",
+        height: "15%",
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        alignItems: "center",
+        marginTop: "8%",
+        marginBottom: "8%"
+    },
+
+    face: {
+        width: 50,
+        height: 50
+    },
+
+    twitter: {
+        width: 50,
+        height: 50
+    },
+
+    insta: {
+        width: 50,
+        height: 50
+    },
+
+    
+});
