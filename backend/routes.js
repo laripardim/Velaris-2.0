38a28f278892d65aa1b5e083717aad4cb315b8c7
Lavínia Sá <82878705+laviniasa@users.noreply.github.com>
2022-06-06 22:43:07 -0300

    Add files via upload

---
+++ backend/routes.js
@@ -0,0 +1,43 @@
+const express = require('express');
+
+const route = express.Router();
+
+const ctrUsuario = require('./src/ctr/ctrUsuario');
+const ctrLivro = require('./src/ctr/ctrLivro');
+const ctrComentario = require('./src/ctr/ctrComentario');
+const ctrEscrita = require('./src/ctr/ctrEscrita.js');
+
+route.post('/login', ctrUsuario.login)
+
+route.post('/usuario', ctrUsuario.create);
+route.get('/usuario', ctrUsuario.read);
+route.get('/usuario/:id', ctrUsuario.read);
+route.put('/usuario/:id', ctrUsuario.update);
+route.delete('/usuario/:id', ctrUsuario.del);
+
+route.post('/livro', ctrLivro.create);
+route.get('/livro', ctrLivro.read);
+route.get('/livro/:id', ctrLivro.read);
+route.post('/livro/titulo', ctrLivro.readtitulo);
+route.put('/livro/:id', ctrLivro.update);
+route.delete('/livro/:id', ctrLivro.del);
+
+
+route.post('/comentario', ctrComentario.create);
+route.get('/comentario', ctrComentario.read);
+route.get('/comentario/:id', ctrComentario.read);
+route.put('/comentario/:id', ctrComentario.update);
+route.delete('/comentario/:id', ctrComentario.del);
+
+
+route.post('/escrita', ctrEscrita.create);
+route.get('/escrita', ctrEscrita.read);
+route.get('/escrita/:id', ctrEscrita.read);
+route.put('/escrita/:id', ctrEscrita.update);
+route.delete('/escrita/:id', ctrEscrita.del);
+
+
+module.exports = route;
+
+
+
