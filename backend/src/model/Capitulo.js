8e5d25e44d01af1514c6b731ce13677ab1f14703
Lavínia Sá <82878705+laviniasa@users.noreply.github.com>
2022-06-11 20:05:06 -0300

    Add files via upload

---
+++ backend/src/model/Capitulo.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+
+class Capitulo extends Model {
+    static init(datacon) {
+        super.init(
+            {
+                texto: {
+                    type: DataTypes.TEXT,
+                    allowNull: true,
+                },
+                id_livro: {
+                    type: DataTypes.INTEGER,
+                    allowNull: false,
+                },
+                capitulo: {
+                    type: DataTypes.INTEGER,
+                    allowNull: false,
+                },
+                titulo_capitulo: {
+                    type: DataTypes.TEXT,
+                    allowNull: true,
+                },
+                
+
+                // id_usuario: {
+                //     type: DataTypes.INTEGER,
+                //     allowNull: false,
+                //     references: {
+                //         model: 'usuario',
+                //         key: 'id',
+                //     }
+                // }
+            },
+            {
+                sequelize: datacon, 
+                tableName: 'capitulos',
+                modelName: 'capitulo',
+            }
+        );
+    }
+
+    static associate (models){
+        // Comentario.hasMany(models.usuario, {foreignKey: 'id_usuario'})
+        // Comentario.hasMany(models.livro, {foreignKey: 'id_livro'})
+    }
+}
+
+module.exports = Capitulo;
